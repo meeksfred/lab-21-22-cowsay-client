f2d9cd0e22ac143654fbfcfec4d8a347bce0cc24
meeksfred <maxfred0025@gmail.com>
2016-10-17 15:46:57 -0700

    working cowsay, 2 bonuses complete

---
+++ lab-max/app/entry.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('./scss/base.scss');
+
+const cowsay = require('cowsay-browser');
+const angular = require('angular');
+
+const cowsayApp = angular.module('cowsayApp', []);
+
+cowsayApp.controller('CowController', ['$log', '$scope', CowController]);
+
+function CowController($log, $scope){
+  $log.debug('init CowController');
+  let cowCtrl = $scope.cowCtrl = {};
+
+  cowCtrl.title = 'Cowsay Lab 2.0, Using Angular';
+  cowCtrl.revertText = [];
+
+  cowCtrl.cowTalk = function(input){
+    $log.debug('cowCtrl.cowTalk');
+    return '\n' + cowsay.say({text: input || 'type an input to make me talk'});
+  };
+
+  cowCtrl.cowPopulate = function(input){
+    $log.debug('cowCtrl.cowPopulate', cowCtrl.revertText);
+    return '\n' + cowsay.say({text: input || 'give me texts'});
+  };
+}
